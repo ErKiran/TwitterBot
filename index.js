f96bc059983e37dce21bef8cbf58ac92e6181d67
Kiran <kiruu1238@gmail.com>
2018-07-19 00:50:17 +0545

    Static embed added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require('./app');
 require('./follow');
+app.use(express.static('public'));
+
 //require('./unsplashapi');
 app.get('/', (req, res) => res.send('This twitter bot is running live at https://twitter.com/CoastQuote'));
 const port = process.env.PORT||5000;
