aba38ac62b205618a7f47f44cfbb1b8bf64da2e6
Kiran <kiruu1238@gmail.com>
2018-07-20 16:14:32 +0545

    Heroku errors attempted to solve

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require('./follow');
 app.use(express.static('public'));
 
 //require('./unsplashapi');
-app.get('/', (req, res) => res.send('This twitter bot is running live at https://twitter.com/CoastQuote'));
 const port = process.env.PORT||5000;
 app.listen(port,()=>{
 	console.log(`Server is running at port ${port}`);
