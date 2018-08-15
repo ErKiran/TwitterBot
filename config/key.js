a5818b6e2a6d57743bb5cf12384be8717c149583
Kiran <enginerkiru@gmail.com>
2018-08-15 14:40:20 +0545

    Travis CI configuration update and key created for CI server

--- config/key.js
+++ config/key.js
@@ -1,5 +1,7 @@
 if(process.env.NODE_ENV ==='production'){
 	module.exports = require('./prod');
-} else{
-	module.exports=require('./dev');
+} else if (process.env.NODE_ENV === 'ci'){
+	module.exports=require('./ci');
+} else {
+	module.exports = require('./dev');
 }
